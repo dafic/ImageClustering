5a1c6610d2ebfc17992b975f36eebd1bbdac6cde
Sandip Pandey <xoriole@gmail.com>
2012-05-31 17:40:11 +0545

    Hbase schema commit

---
+++ ImageClustering/src/main/java/com/ioe/imageclustering/imageclustering/App.java
@@ -0,0 +1,13 @@
+package com.ioe.imageclustering.imageclustering;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!1" );
+    }
+}
